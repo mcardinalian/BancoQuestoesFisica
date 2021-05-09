7416b9acdcb1ea0d128b854a965a95dbafb060ef
Marcelo Cardinali <mcardinalian@gmail.com>
2021-05-09 12:10:59 -0300

    Update ExerciciosParaMoodle.R

--- Moodle/ExerciciosParaMoodle.R
+++ Moodle/ExerciciosParaMoodle.R
@@ -482,3 +482,18 @@ exams2moodle(myexam, n = 1, rule="none",
              encoding = "UTF-8",
              dir = "./Moodle",
              edir = paste0("./BancoDeQuestoes/eletromagnetismo/eletrostatica/",assunto))
+
+##########Lei de Coulomb ##################
+## Definindo a pasta com as questoes do exame
+assunto = "leidecoulomb"
+myexam <- dir(paste0("./BancoDeQuestoes/eletromagnetismo/",assunto), pattern = ".rnw", ignore.case=TRUE)
+ano <- 12021
+## Cria o arquivo .xml para entrada no moodle
+set.seed(ano)
+exams2moodle(myexam, n = 60, rule="none", 
+             schoice = list(shuffle = TRUE), 
+             converter = "pandoc-mathjax",
+             name = paste0(assunto,"-",ano),
+             encoding = "UTF-8",
+             dir = "./Moodle",
+             edir = paste0("./BancoDeQuestoes/eletromagnetismo/",assunto))
